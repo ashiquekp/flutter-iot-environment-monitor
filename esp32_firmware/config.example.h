37346f49b0d9b71f961814b7c35f2f4f27baa20e
ashiquekp <ashiquekp101@gmail.com>
2026-06-08 18:17:09 +0530

    Add MQTT configuration

--- esp32_firmware/config.example.h
+++ esp32_firmware/config.example.h
@@ -1,7 +1,18 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-const char* WIFI_SSID = "YOUR_WIFI_NAME";
-const char* WIFI_PASSWORD = "YOUR_WIFI_PASSWORD"; 
+constexpr const char* WIFI_SSID = "YOUR_WIFI_NAME";
+constexpr const char* WIFI_PASSWORD = "YOUR_WIFI_PASSWORD";
+
+constexpr const char* MQTT_BROKER = "broker.hivemq.com";
+constexpr int MQTT_PORT = 1883;
+
+constexpr const char* DEVICE_ID = "esp32-c3-001";
+
+constexpr const char* TOPIC_ENVIRONMENT =
+  "home/lab/device1/environment";
+
+constexpr const char* TOPIC_STATUS =
+  "home/lab/device1/status";
 
 #endif
