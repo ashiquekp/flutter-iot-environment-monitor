76938d206c0a31f88fd53068c17be6173e551f1d
ashiquekp <ashiquekp101@gmail.com>
2026-06-08 18:17:56 +0530

    Publish DHT11 readings over MQTT

--- esp32_firmware/esp32_firmware.ino
+++ esp32_firmware/esp32_firmware.ino
@@ -1,28 +1,59 @@
 #include "sensor_manager.h"
 #include "wifi_manager.h"
+#include "mqtt_manager.h"
+
+unsigned long lastPublishTime = 0;
 
 void setup() {
+
   Serial.begin(115200);
 
   initSensor();
 
   initWiFi();
+
+  initMQTT();
+
+  Serial.println("System Ready");
 }
 
 void loop() {
+
   handleWiFi();
 
-  SensorData data = readSensor();
+  handleMQTT();
 
-  if (data.isValid) {
-    Serial.print("Temperature: ");
-    Serial.println(data.temperature);
+  unsigned long currentTime = millis();
 
-    Serial.print("Humidity: ");
-    Serial.println(data.humidity);
+  if (
+    currentTime - lastPublishTime >= 5000
+  ) {
+
+    lastPublishTime = currentTime;
+
+    SensorData data = readSensor();
+
+    if (data.isValid) {
+
+      Serial.print("Temperature: ");
+      Serial.println(data.temperature);
+
+      Serial.print("Humidity: ");
+      Serial.println(data.humidity);
+
+      publishSensorData(
+        data.temperature,
+        data.humidity
+      );
+    } else {
+
+      Serial.println(
+        "Invalid DHT11 reading"
+      );
+    }
+
+    Serial.println(
+      "---------------------"
+    );
   }
-
-  Serial.println("----------------");
-
-  delay(5000);
 }
