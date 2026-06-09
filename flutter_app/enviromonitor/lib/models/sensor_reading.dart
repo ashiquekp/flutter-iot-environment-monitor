7d8f9566ebc32689a82f54e9217461e8e3f4cb0b
ashiquekp <ashiquekp101@gmail.com>
2026-06-09 13:12:10 +0530

    Add sensor reading model

---
+++ flutter_app/enviromonitor/lib/models/sensor_reading.dart
@@ -0,0 +1,23 @@
+class SensorReading {
+  final String deviceId;
+  final double temperature;
+  final double humidity;
+
+  const SensorReading({
+    required this.deviceId,
+    required this.temperature,
+    required this.humidity,
+  });
+
+  factory SensorReading.fromJson(
+    Map<String, dynamic> json,
+  ) {
+    return SensorReading(
+      deviceId: json['deviceId'] as String,
+      temperature:
+          (json['temperature'] as num).toDouble(),
+      humidity:
+          (json['humidity'] as num).toDouble(),
+    );
+  }
+}
