b666ff30b59855dc94506c83f34925c5fb60c7f1
ashiquekp <ashiquekp101@gmail.com>
2026-06-09 12:58:19 +0530

    Initialize Flutter application

--- esp32_firmware/config.h
+++ esp32_firmware/config.h
@@ -1,7 +1,18 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-const char* WIFI_SSID = "realme 10 Pro 5G";
-const char* WIFI_PASSWORD = "Ashique.";
+constexpr const char* WIFI_SSID = "realme 10 Pro 5G";
+constexpr const char* WIFI_PASSWORD = "Ashique.";
+
+constexpr const char* MQTT_BROKER = "broker.hivemq.com";
+constexpr int MQTT_PORT = 1883;
+
+constexpr const char* DEVICE_ID = "esp32-c3-001";
+
+constexpr const char* TOPIC_ENVIRONMENT =
+  "home/lab/device1/environment";
+
+constexpr const char* TOPIC_STATUS =
+  "home/lab/device1/status";
 
 #endif
